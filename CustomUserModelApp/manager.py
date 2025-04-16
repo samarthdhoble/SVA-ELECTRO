6912d7e6dff7842266b754b7fdd03b9b0cfbeb54
Sanchit Kumbhar <sanchitskumbhar@gmail.com>
2025-04-16 22:00:24 +0530

    auth model  created

--- CustomUserModelApp/manager.py
+++ CustomUserModelApp/manager.py
@@ -1,20 +1,22 @@
 from django.contrib.auth.base_user import BaseUserManager
 
 class CustomUserManager(BaseUserManager):
-    def create_user(self, phonenumber, email, password, fullname, **extra_fields):
+    def create_user(self, phonenumber, email, password, **extra_fields):
         if not email:
             raise ValueError("The Email field must be set")
         email = self.normalize_email(email)
-        user = self.model(phonenumber=phonenumber, email=email, fullname=fullname, **extra_fields)
+        user = self.model(phonenumber=phonenumber, email=email, **extra_fields)
         user.set_password(password)
         user.save(using=self._db)
         return user
 
-    def create_superuser(self, phonenumber, email, password, fullname, **extra_fields):
+    def create_superuser(self, phonenumber, email, password, **extra_fields):
         extra_fields.setdefault("is_staff", True)
         extra_fields.setdefault("is_superuser", True)
         extra_fields.setdefault("is_active", True)
 
         if not email:
             raise ValueError("The Email field must be set for superuser")
-        return self.create_user(phonenumber=phonenumber, email=email, password=password, fullname=fullname, **extra_fields)
+        return self.create_user(phonenumber=phonenumber, email=email, password=password, **extra_fields)
+    
+
--- CustomUserModelApp/models.py
+++ CustomUserModelApp/models.py
@@ -1,15 +1,42 @@
+from django.conf import  settings
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 # Create your models here.
 from .manager import CustomUserManager
 
 class CustomUser(AbstractUser):
+    username=None
     email=models.EmailField(unique=True)
-    fullname=models.CharField(max_length=100,blank=False)
     phonenumber=models.CharField(max_length=10,unique=True)
 
 
     USERNAME_FIELD = 'email'  # This should match the unique identifier for your user
-    REQUIRED_FIELDS = ['phonenumber', 'fullname']  # Fields required for superuser creation
+    REQUIRED_FIELDS = ['phonenumber']  # Fields required for superuser creation
 
     objects = CustomUserManager()
+
+class GovernmentDetails(models.Model):
+    login = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    department=models.CharField(max_length=122)
+    gov_id=models.CharField(max_length=122)
+
+class PrivateBussines(models.Model):
+    login = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    business_name=models.CharField(max_length=122)
+    registration_no=models.IntegerField(blank=False)
+    phone_number=models.CharField(max_length=10,unique=True)
+    address=models.TextField()
+
+class Client(models.Model):
+    login = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    fullname=models.CharField(max_length=122)
+    phone_number=models.CharField(max_length=10,unique=True)
+    
+
+
+#     email
+#     password
+#     -------->
+#             B2B
+#             B2C
+#             B2G
