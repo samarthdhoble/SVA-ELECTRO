6fbc46f2b3f355cc0cc6dd831d375d37cb738ee2
Sanchit Kumbhar <sanchitskumbhar@gmail.com>
2025-05-13 01:03:31 +0530

    ongoing

--- Backend/products/models.py
+++ Backend/products/models.py
@@ -1,3 +1,21 @@
 from django.db import models
-
+from django.conf import settings
 # Create your models here.
+class Product(models.Model):
+
+    SLOT_CHOICES=[
+        ('A',"Afternoon"),
+        ('E','Evening'),
+        ('M','Morning')
+    ]
+
+    vehicleimage = models.ImageField(upload_to="vehicleimg")
+    modelname=models.CharField(max_length=50)
+    description=models.TextField()
+    qty=models.IntegerField()
+    user = models.ForeignKey(settings.AUTH_USER_MODEL,on_delete=models.CASCADE)
+    appointmentdate=models.DateField(null=True)
+    slot=models.CharField(max_length=2,choices=SLOT_CHOICES,default='A')
+    location=models.CharField(max_length=50)
+    purpose=models.CharField(max_length=50)
+    message=models.TextField()
--- Backend/products/views.py
+++ Backend/products/views.py
@@ -1,5 +1,29 @@
 from django.shortcuts import render
-
+from django.http import JsonResponse
+from .models import Product
 # Create your views here.
 def productspage(request):
     return render(request,"products.html")
+
+def submitProductpage(request):
+    vehicleimage=request.FILES['vehicleimg']
+    modelname=request.POST.get('modelname')
+    description=request.POST.get('description')
+    qty=request.POST.get('qty')
+    appointmentdate=request.POST.get('aptdate')
+    location=request.POST.get("location")
+    slot=request.POST.get('slot')
+    purpose=request.POST.get('purpose')
+    message=request.POST.get('message')
+    user=request.user
+
+    data=Product.objects.create(
+        vehicleimage=vehicleimage,modelname=modelname,user=user,description
+        =description,qty=qty,appointmentdate=appointmentdate,slot=slot,location=location,purpose=purpose,message=message
+    )
+
+    print(data)
+
+    return JsonResponse({
+        'response'  : 200
+    })
